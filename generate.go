cacd5998fd468004ceef094b50157509dad856f4
Connor Edwards <38229097+cedws@users.noreply.github.com>
2025-05-08 20:24:44 +0900

    Add run-actions API

--- generate.go
+++ generate.go
@@ -5,3 +5,5 @@ package api
 //go:generate go tool oapi-codegen -generate types,client -package pipelines -o actions/pipelines/client.gen.go schemas/pipelines.actions.githubusercontent.com.yaml
 
 //go:generate go tool oapi-codegen -generate types,client -package results -o actions/results/client.gen.go schemas/results-receiver.actions.githubusercontent.com.yaml
+
+//go:generate go tool oapi-codegen -generate types,client -package run -o actions/run/client.gen.go schemas/run-actions.actions.githubusercontent.com.yaml
